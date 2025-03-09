d11ec333f259b21a49232d1639ddfa311b767170
Dev-GP <penchev.dev@gmail.com>
2025-03-09 23:47:20 +0200

    project init
    project init

---
+++ TrafficLightSystem/TrafficLightSystem/TrafficLightSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSystemApp.swift
+//  TrafficLightSystem
+//
+//  Created by Georgi Penchev on 9.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
